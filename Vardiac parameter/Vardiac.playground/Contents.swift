00d040969c9dfef3c351b8ab72687f460f0c66f7
mumthezirvp <mumthezir@gmail.com>
2024-07-03 23:14:55 +0530

    Fix: Fixed spelling

--- Vardiac parameter/Vardiac.playground/Contents.swift
+++ Vardiac parameter/Vardiac.playground/Contents.swift
@@ -1,12 +1,12 @@
 import UIKit
 
-class VardiacClass {
-    static func varidiacParameterContainingMethod(vardiacParam: Int...) -> Int {
+class VariadicClass {
+    static func variadicParameterContainingMethod(vardiacParam: Int...) -> Int {
         return vardiacParam.reduce(0, +)
     }
 }
 
-// Vardiac parameter with multiple integer elements
-VardiacClass.varidiacParameterContainingMethod(vardiacParam: 1,2,3,4,5)
-// Vardiac parameter with single integer element
-VardiacClass.varidiacParameterContainingMethod(vardiacParam: 10)
+// Variadic parameter with multiple integer elements
+VariadicClass.variadicParameterContainingMethod(vardiacParam: 1,2,3,4,5)
+// Variadic parameter with single integer element
+VariadicClass.variadicParameterContainingMethod(vardiacParam: 10)
